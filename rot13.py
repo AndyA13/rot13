34cb3922b96a3d30a3064febe78eecaea4527bb3
Andrew Aitken <hermanthesheep@gmail.com>
2014-04-02 21:07:58 +0100

    Bit of refactoring

--- rot13.py
+++ rot13.py
@@ -6,30 +6,30 @@ class Rot13(object):
 
         rot_map = {}
 
-        for c in range(65, 91): # A to Z
-
-            if c > 77:
-                rot_map[chr(c)] = chr(c - 13)
-            else:
-                rot_map[chr(c)] = chr(c + 13)
-
-        for c in range(97, 123): # a to z
-
-            if c > 109:
-                rot_map[chr(c)] = chr(c - 13)
-            else:
-                rot_map[chr(c)] = chr(c + 13)
+        # a to z
+        self.add_to_dictionary(rot_map, 65, 91)
+        
+        # A to Z
+        self.add_to_dictionary(rot_map, 97, 123)
 
         output = ""
 
         for character in input_text:
 
-            if character in rot_map:
-                output = output + rot_map[character]
-            else:
-                output = output + character
+            output = output + rot_map.get(character, character)
 
         return output
+
+    def add_to_dictionary(self, rot_map, start, end):
+
+        split_point = start + ((end - start) / 2)
+
+        for ascii_code in range(start, end):
+
+            if ascii_code >= split_point:
+                rot_map[chr(ascii_code)] = chr(ascii_code - 13)
+            else:
+                rot_map[chr(ascii_code)] = chr(ascii_code + 13)
 
 if __name__ == "__main__":
     rot13 = Rot13()
---
+++ tests.py
@@ -0,0 +1,44 @@
+
+import unittest
+
+from rot13 import Rot13
+
+class Tests(unittest.TestCase):
+
+    def test_add_to_dictionary(self):
+
+        # add the lower case alhpabet
+        encryptor = Rot13()
+
+        test_dict = {}
+
+        encryptor.add_to_dictionary(test_dict, 97, 123)
+        
+        self.assertEqual(26, len(test_dict))
+
+        # test a few points and edge cases
+        self.assertEqual("n", test_dict["a"])
+        self.assertEqual("q", test_dict["d"])
+        self.assertEqual("z", test_dict["m"])
+        self.assertEqual("a", test_dict["n"])
+        self.assertEqual("g", test_dict["t"])
+        self.assertEqual("m", test_dict["z"])
+
+    def test_rot13_encryption(self):
+
+        encryptor = Rot13()
+        
+        test_input = "My test string."
+
+        output = encryptor.translate(test_input)
+
+        # string should be rot13 "encrypted"
+        self.assertNotEqual(output, test_input)
+
+        output = encryptor.translate(output)
+
+        # applying rot13 twice should reverse the "encryption"
+        self.assertEqual(output, test_input)
+
+if __name__ == "__main__":
+    unittest.main()
